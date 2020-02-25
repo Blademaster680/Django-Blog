aad54962eeba8f31199444ce15d7f5a21d6e6a8f
Blademaster680 <dylanprimic@gmail.com>
2020-02-25 13:03:23 +0200

    User Profile and Picture added
    Python Django Tutorial: Full-Featured Web App Part 8 - User Profile and Picture completed.
    
    User profile is now auto created when a new user is created.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    # Setting User model and Profile model one to one so that 1 user has 1 profile and vice versa
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Setting the user's profile and uploaded path
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
